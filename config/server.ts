59f6ff56fcdca433e86d85a688952427c13830c8
batistacaceres <batistacaceres@gmail.com>
2025-10-10 19:41:14 +0200

    strapi v2, COOKIE_SECURE= false

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,15 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+    cookie: {
+      secure: env.bool("COOKIE_SECURE", false),
+    },
   },
 });
